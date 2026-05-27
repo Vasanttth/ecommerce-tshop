3db54bf9142a4e1ddc5dfda005a8eccafa6ae26e
vasanttth <vasanthansanthavel@gmail.com>
2026-05-27 11:38:51 +0530

    Fix deploy by removing bootstrap imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 import Home from "./pages/Home";
 import About from "./pages/About";
