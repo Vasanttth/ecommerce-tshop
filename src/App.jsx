f4698d61c3ca14d030b8285b62b9ddbd70113392
vasanttth <vasanthansanthavel@gmail.com>
2026-05-27 11:33:33 +0530

    Make ecommerce pages mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
